f131c5caf78e1fbc5adafb6602c0569c5be2a835
ditinex <asifakramsk@gmail.com>
2021-08-23 16:13:16 +0530

    home page completed

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -4,11 +4,12 @@ import {
     Image,
     Text,
     TouchableOpacity,
-    TextInput
+    TextInput,
+    FlatList
 } from 'react-native';
 import style from './style'
 import Config from 'src/config'
-import { logo_white, hamburger } from 'src/assets'
+import { logo_white, background, about, courses, research, videos } from 'src/assets'
 import API from 'src/services/api'
 import * as ACTION from 'src/reduxData/action'
 import { useDispatch, useSelector } from 'react-redux'
@@ -19,29 +20,63 @@ import Icon2 from 'react-native-vector-icons/Entypo'
 import LinearGradient from 'react-native-linear-gradient'
 
 
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'About WilHub',
+        image: about
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Courses',
+        image: courses
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Research',
+        image: research
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d73',
+        title: 'Videos',
+        image: videos
+    },
+];
+
+
 export default ({ navigation }) => {
     const [Colors, styles] = useTheme(style)
     const translate = useLanguage().t
     const dispatch = useDispatch()
     const sessionReducer = useSelector(state => state.sessionReducer)
-    const [searchQuery,setSearchQuery] = useState()
+    const [searchQuery, setSearchQuery] = useState()
 
     useEffect(() => {
 
     }, [])
 
+    const renderItem = ({ item }) => (
+        <View style={styles.itemContainer}>
+            <TouchableOpacity style={[styles.flex1,styles.centerAll]}>
+            <Image style={styles.image} source={item.image} />
+            <Text style={styles.title}>{item.title}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
     return (
         <Container isTransparentStatusBar={false}>
+            <Image source={background} style={styles.background} />
             <LinearGradient colors={[Colors.primary, Colors.secondary]} style={styles.headerBar}>
                 <Image source={logo_white} style={styles.logo} />
-                <View style={[styles.flexRow,styles.centerAll]}>
-                    <TouchableOpacity onPress={()=>navigation.toggleDrawer()}>
+                <View style={[styles.flexRow, styles.centerAll]}>
+                    <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
                         <Icon2 name={'menu'} size={60} color={Colors.white} />
                     </TouchableOpacity>
                     <View style={styles.searchHolder}>
                         <TextInput
                             style={styles.searchInput}
-                            onChangeText={(text)=>setSearchQuery(text)}
+                            onChangeText={(text) => setSearchQuery(text)}
                             value={searchQuery}
                             placeholder="What do you wnat to learn today ?"
                         />
@@ -51,6 +86,14 @@ export default ({ navigation }) => {
                     </View>
                 </View>
             </LinearGradient>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                numColumns={2}
+                ListHeaderComponent={<View style={styles.marginTop30}></View>}
+                ListFooterComponent={<View style={styles.marginBottom80}></View>}
+            />
         </Container>
     )
 }
--- src/screens/Dashboard/style.js
+++ src/screens/Dashboard/style.js
@@ -30,6 +30,26 @@ const styles =({Colors})=> StyleSheet.create({
         flex: 1,
         marginVertical: 0,
         paddingVertical: 0
+    },
+    background:{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: Mixins.DEVICE_WIDTH,
+        height: Mixins.DEVICE_HEIGHT
+    },
+    itemContainer:{
+        flex: 1,
+        alignItems: 'center'
+    },
+    image: {
+        width: Mixins.DEVICE_WIDTH/2,
+        height: Mixins.scaleSize(230),
+        resizeMode: 'stretch'
+    },
+    title:{
+        fontSize: Typography.FONT_SIZE_20,
+        color: Colors.primary
     }
 });
 
