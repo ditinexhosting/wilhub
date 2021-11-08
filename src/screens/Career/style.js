efb024d5d41e396ae903574816e3643646bccae4
SuryaKarmakar <surjokarmakarssj7@gmail.com>
2021-11-08 16:10:56 +0530

    career screen ui

--- src/screens/Career/style.js
+++ src/screens/Career/style.js
@@ -1,47 +1,74 @@
-import {
-    StyleSheet,
-} from 'react-native';
-import { Mixins, Spacing, Typography,style } from 'src/styles'
+import {StyleSheet} from 'react-native';
+import {Mixins, Spacing, Typography, style} from 'src/styles';
 
-const styles =({Colors})=> StyleSheet.create({
-    header:{
-       marginVertical: Spacing.SCALE_15,
+const styles = ({Colors}) =>
+  StyleSheet.create({
+    headerBar: {
+      height: Mixins.scaleSize(85),
+      borderBottomLeftRadius: Spacing.SCALE_40,
+      borderBottomRightRadius: Spacing.SCALE_40,
+      justifyContent: 'center',
     },
-    backButton:{
-        position: 'absolute',
-        top: Spacing.SCALE_20,
-        left: Spacing.SCALE_20,
-        zIndex: 999
+    background: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: Mixins.DEVICE_WIDTH,
+      height: Mixins.DEVICE_HEIGHT,
     },
-    headerText:{
-        fontSize: Typography.FONT_SIZE_25,
-        color:Colors.white,
-        flex: 1,
-        textAlign: 'center'
+    backButton: {
+      position: 'absolute',
+      top: Spacing.SCALE_20,
+      left: Spacing.SCALE_20,
+      zIndex: 1,
     },
-    titleText:{
-        fontSize: Typography.FONT_SIZE_25,
-        color: Colors.gray_dark,
-        flex: 1,
-        textAlign: 'center',
-        marginVertical: Spacing.SCALE_10
+    headerText: {
+      marginVertical: Spacing.SCALE_15,
+      fontSize: Typography.FONT_SIZE_20,
+      color: Colors.white,
+      flex: 1,
+      textAlign: 'center',
+      fontWeight: '500',
     },
-    background:{
-        flex: 1
+    weAreHiringImgView: {
+      width: '100%',
+      height: 180,
+      borderRadius: 20,
+      marginTop: Spacing.SCALE_18,
+      marginBottom: Spacing.SCALE_10,
     },
-    logo:{
-        width: Mixins.DEVICE_WIDTH - 20,
-        height: Mixins.DEVICE_WIDTH - 20,
-        resizeMode: 'cover',
-        alignSelf: 'center',
+    cardView: {
+      width: '100%',
+      height: 145,
+      backgroundColor: Colors.gray_medium,
+      marginVertical: 10,
+      borderRadius: 10,
     },
-    descriptionText:{
-        color:Colors.white,
-        textAlign:'justify',
-        marginHorizontal:Spacing.SCALE_20,
-        fontSize:Typography.FONT_SIZE_15,
-        marginVertical: Spacing.SCALE_10
-    }
-})
+    cardTopView: {
+      width: '100%',
+      height: '29%',
+      justifyContent: 'center',
+      paddingLeft: Spacing.SCALE_8,
+    },
+    cardCenterView: {
+      width: '100%',
+      height: '50%',
+      paddingLeft: Spacing.SCALE_8,
+    },
+    cardBottomView: {
+      width: '100%',
+      height: '21%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    applyBtnView: {
+      width: 120,
+      height: 18,
+      backgroundColor: Colors.primary,
+      borderRadius: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
-export default styles
+export default styles;
