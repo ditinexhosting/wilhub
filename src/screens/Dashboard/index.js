b1559d906b0d32e633d98034d8130b1a5560044f
Surya Karmakar <74616729+SuryaKarmakar@users.noreply.github.com>
2021-12-22 22:49:06 +0530

    Revert "payment"

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -8,7 +8,6 @@ import {
   FlatList,
   TouchableWithoutFeedback,
   StatusBar,
-  Button,
 } from 'react-native';
 import style from './style';
 import Config from 'src/config';
@@ -103,17 +102,13 @@ export default ({navigation}) => {
           </View>
         </View>
       </LinearGradient>
-      {/* <FlatList
+      <FlatList
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         numColumns={2}
         ListHeaderComponent={<View style={styles.marginTop30}></View>}
         ListFooterComponent={<View style={styles.marginBottom100}></View>}
-      /> */}
-      <Button
-        title={'testing'}
-        onPress={() => navigation.navigate('PaymentScreen')}
       />
     </Container>
   );
